931b26ebc9fcf6e67ee633323a1e5bec83615c30
jswz72 <jacobdasword@gmail.com>
2017-11-05 09:15:33 -0500

    working bar chart

--- app/components/poll-view.vue
+++ app/components/poll-view.vue
@@ -7,11 +7,13 @@
       </li>
     </ul>
     <button class="button is-success" v-on:click="vote">Submit</button>
+    <canvas id="chart" width="250" height="250"></canvas>
   </div>
 </template>
 
 <script>
   import controller from '../controllers/clickController.client'
+  import Chart from 'chart.js'
   export default {
     name: 'poll-view',
     props: {
@@ -26,6 +28,17 @@
         userVoted: false
       }
     },
+    mounted () {
+      this.populateChart();
+    },
+    computed: {
+      labels () {
+        return this.poll.options.map(option => option.name);
+      },
+      votesPerLabel () {
+        return this.poll.options.map(option => option.votes);
+      }
+    },
     methods: {
       vote () {
         if (this.voteOption === '') {
@@ -33,6 +46,40 @@
         }
         controller.vote(this.poll.title, this.voteOption);
         this.userVoted = true;
+      },
+      populateChart () {
+        const ctx = document.getElementById('chart');
+        //eslint-disable-next-line
+        let pollChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: this.labels,
+            datasets: [{
+              label: '# of Votes',
+              data: this.votesPerLabel,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)'
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            },
+            responsive: false,
+            maintainAspectRatio: false
+          }
+        })
       }
     }
   }
